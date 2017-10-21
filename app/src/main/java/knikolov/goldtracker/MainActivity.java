e7cd8d91aa3c944d3028bb7679745dd7937d1d4c
sleepyeldrazi <konik98@gmail.com>
2017-10-21 23:33:16 +0200

    Needed push

--- app/src/main/java/knikolov/goldtracker/MainActivity.java
+++ app/src/main/java/knikolov/goldtracker/MainActivity.java
@@ -10,6 +10,7 @@ import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
